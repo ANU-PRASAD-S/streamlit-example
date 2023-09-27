37032eec30d8c97eef222b12da285a953efc3d2b
ANU-PRASAD-S <143721871+ANU-PRASAD-S@users.noreply.github.com>
2023-09-27 15:24:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,87 @@
-from collections import namedtuple
-import altair as alt
-import math
+import os
 import pandas as pd
 import streamlit as st
+from googleapiclient.discovery import build
+import re
 
-"""
-# Welcome to Streamlit!
+# Initialize the YouTube Data API client
+API_KEY = "AIzaSyA2t7_3fcDsDA00drph9nRERsI-QPnXgrQ"
+youtube = build("youtube", "v3", developerKey=API_KEY)
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Streamlit app title
+st.title("YouTube Data Extractor")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Create a folder to save CSV files
+output_folder = "output_data"
+os.makedirs(output_folder, exist_ok=True)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Input for YouTube URL
+youtube_url = st.text_input("Enter a YouTube URL:")
 
+if st.button("Get Channel Data"):
+    if youtube_url:
+        # Extract channel ID from the URL
+        match = re.search(r"channel/([A-Za-z0-9_-]+)", youtube_url)
+        if match:
+            channel_id = match.group(1)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+            # Create CSV file name based on channel ID
+            csv_filename = os.path.join(output_folder, f"{channel_id}_channel_data.csv")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+            # Fetch channel data
+            channel_request = youtube.channels().list(
+                part="snippet,statistics,status",
+                id=channel_id,
+            )
+            channel_response = channel_request.execute()
 
-    points_per_turn = total_points / num_turns
+            if "items" in channel_response:
+                channel_data = channel_response["items"][0]
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+                # Extract channel data
+                channel_name = channel_data["snippet"]["title"]
+                channel_type = channel_data["snippet"]["customUrl"]
+                channel_views = channel_data["statistics"]["viewCount"]
+                channel_description = channel_data["snippet"]["description"]
+                channel_status = channel_data["status"]["privacyStatus"]
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+                # Create a DataFrame for channel data
+                channel_df = pd.DataFrame({
+                    "channel_name": [channel_name],
+                    "channel_type": [channel_type],
+                    "channel_views": [channel_views],
+                    "channel_description": [channel_description],
+                    "channel_status": [channel_status],
+                })
+
+                # Save channel data to CSV
+                channel_df.to_csv(csv_filename, index=False)
+
+                st.success(f"Channel data saved to {csv_filename}")
+        else:
+            st.error("No channel ID found in the provided URL.")
+
+# Display the list of available CSV files
+csv_files = os.listdir(output_folder)
+csv_files = [f for f in csv_files if f.endswith("_channel_data.csv")]
+
+if csv_files:
+    st.subheader("Available Channel Data CSVs")
+    selected_csv = st.selectbox("Select a CSV:", csv_files)
+
+    # Read and display the selected CSV
+    selected_csv_path = os.path.join(output_folder, selected_csv)
+    selected_data = pd.read_csv(selected_csv_path)
+    st.write(selected_data)
+else:
+    st.warning("No CSV files available.")
+
+# Information about how to use the app
+st.markdown(
+    """
+    **Instructions:**
+    1. Enter a valid YouTube URL in the input box above.
+    2. Click the 'Get Channel Data' button to fetch channel data and save it to a CSV file.
+    3. The saved CSV files can be selected from the dropdown list to view their contents.
+    """
+)
